3eaf63c6b0c186fc0810152c314d84af3ebabfee
Shubham Singh <coolsks94@gmail.com>
2018-05-29 12:22:41 +0530

    Allow to use overlays

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 LOCAL_PACKAGE_NAME := Stk
 LOCAL_CERTIFICATE := platform
 
